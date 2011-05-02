38f6c70fa20556e96c5c28be3bf20ce04e83beb8
bill <bill.mill@gmail.com>
2011-05-02 14:57:00 -0400

    adding the registration index view

--- cmua/registration/views.py
+++ cmua/registration/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+from registration.models import Register
+from registration.forms import RegistrationForm
+
+def index(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            #TODO: process the form
+            return HttpResponseRedirect('/register/thanks.html')
+    else:
+        form = RegistrationForm()
+    print "form: --------", repr(form)
+    return render_to_response("register/index.html",
+                              {'form': form},
+                              context_instance=RequestContext(request))
