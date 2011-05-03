6c24c4b8a5b32f97815c29bc18b8e338f892d3ac
bill <bill.mill@gmail.com>
2011-05-03 09:28:58 -0400

    adding directions page

--- cmua/urls.py
+++ cmua/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^register/', include('cmua.registration.urls')),
+
+    (r'^directions$', direct_to_template, {'template': "directions.html"}),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
