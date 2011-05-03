4c71c703c04bc7e5863564d9780e687c1df656ad
bill <bill.mill@gmail.com>
2011-05-02 23:31:28 -0400

    passing the registrant's name to the checkout page and adding it as the item description

--- cmua/registration/urls.py
+++ cmua/registration/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', "registration.views.index", name="register"),
-    url(r'^checkout.html$', "registration.views.checkout", name="checkout"),
+    url(r'^checkout.html/(?P<name>.*)$', "registration.views.checkout", name="checkout"),
 )
--- cmua/registration/views.py
+++ cmua/registration/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 
 from registration.models import Register
 from registration.forms import RegistrationForm
@@ -10,7 +10,8 @@ def index(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             #TODO: process the form, .save() the instance (see ModelForm docs)
-            return HttpResponseRedirect('/register/checkout.html')
+            name = form.data['first_name'] + " " + form.data["last_name"]
+            return redirect('checkout', name=name)
     else:
         form = RegistrationForm()
     print "form: --------", repr(form)
@@ -18,6 +19,6 @@ def index(request):
                               {'form': form},
                               context_instance=RequestContext(request))
 
-def checkout(request):
-    return render_to_response("register/checkout.html", {},
+def checkout(request, name=""):
+    return render_to_response("register/checkout.html", {'name':name},
                               context_instance=RequestContext(request))
