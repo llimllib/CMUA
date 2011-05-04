4273bfa73fd9211812555b7da1b6f0fea8407abf
bill <bill.mill@gmail.com>
2011-05-04 15:18:46 -0400

    sending an email upon registration

--- cmua/registration/views.py
+++ cmua/registration/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
+from django.core.mail import send_mail
 from django.shortcuts import render_to_response, redirect
+
+from urllib import quote
 
 from cmua.registration.models import Register
 from cmua.registration.forms import RegistrationForm
@@ -12,6 +15,21 @@ def index(request):
             form.save()
 
             name = form.data['first_name'] + " " + form.data["last_name"]
+
+            send_mail("Thanks for registering with CMUA",
+"""%s,
+
+Thank you for registering for CMUA 2011.
+
+If you haven't yet paid for the league, you can do so by visiting
+http://md-ultimate.org/register/checkout.html/%s and clicking the
+google payment button or following the instructions for mailing in
+a check.
+
+Have a great season.""" % (name, quote(name)),
+                      'cmua@md-ultimate.org',
+                      [form.data['email_address']],
+                      fail_silently=True)
             return redirect('checkout', name=name)
     else:
         form = RegistrationForm()
