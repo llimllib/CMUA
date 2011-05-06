8b3b891da1a7de487cea112b68244e69f2515c3c
bill <bill.mill@gmail.com>
2011-05-06 12:39:57 -0400

    adding thursday league to the form validation

--- cmua/registration/forms.py
+++ cmua/registration/forms.py
@@ -6,10 +6,11 @@ class RegistrationForm(forms.ModelForm):
     def clean(self):
         monday = self.cleaned_data.get("monday_league")
         wednesday = self.cleaned_data.get("wednesday_league")
+        thursday = self.cleaned_data.get("thursday_league")
 
         errors = []
-        if not monday and not wednesday:
-            errors.append("You must select either Monday or Wednesday league")
+        if not monday and not wednesday and not thursday:
+            errors.append("You must select at least one league to play in")
 
         if not self.cleaned_data.get("i_agree_to_terms"):
             errors.append("You must agree to the terms of the waiver")
