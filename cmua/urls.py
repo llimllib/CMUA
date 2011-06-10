c5ad67822503507e73a809a39dbaaf5ecc124f9e
bill <bill.mill@gmail.com>
2011-06-10 00:43:10 -0400

    adding blog urls and scorereporter urls

--- cmua/urls.py
+++ cmua/urls.py
@@ -6,11 +6,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^register/', include('cmua.registration.urls')),
+    (r'^report_score/', include('cmua.scorereporter.urls')),
 
     (r'^directions$', direct_to_template, {'template': "directions.html"}),
 
     (r'^admin/(?P<app_label>[\d\w]+)/(?P<model_name>[\d\w]+)/csv/', 'cmua.csv_view.actions.admin_list_export'),
     (r'^admin/', include(admin.site.urls)),
 
+    (r'^blog/', include("cmua.nassau.urls")),
     (r'^$', "cmua.nassau.views.index"),
 )
