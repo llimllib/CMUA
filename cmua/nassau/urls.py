3fefbecb5fab67a992fc0c76bc87582a52f74553
bill <bill.mill@gmail.com>
2011-06-10 00:41:19 -0400

    absolutizing nassau urls

--- cmua/nassau/urls.py
+++ cmua/nassau/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns("",
-    url(r'^$', "nassau.views.index", name="blog"),
-    url(r'^archive$', "nassau.views.list", name="blog_list"),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', "nassau.views.blog_post", name="blog_post"),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', "nassau.views.archive", name="blog_archive"),
-    url(r'^(?P<year>\d{4})/$', "nassau.views.archive"),
-    url(r'^category/(?P<category>[-\w ]+)/$', "nassau.views.category"),
-    url(r'^mce_list$', "nassau.views.mce_list", name="images"),
-    url(r'^feed', "nassau.views.feed", name="blog_feed"),
+    url(r'^$', "cmua.nassau.views.index", name="blog"),
+    url(r'^archive$', "cmua.nassau.views.list", name="blog_list"),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', "cmua.nassau.views.blog_post", name="blog_post"),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', "cmua.nassau.views.archive", name="blog_archive"),
+    url(r'^(?P<year>\d{4})/$', "cmua.nassau.views.archive"),
+    url(r'^category/(?P<category>[-\w ]+)/$', "cmua.nassau.views.category"),
+    url(r'^mce_list$', "cmua.nassau.views.mce_list", name="images"),
+    url(r'^feed', "cmua.nassau.views.feed", name="blog_feed"),
 )
