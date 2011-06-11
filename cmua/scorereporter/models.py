f39a4a23eb3ff2b64d848a387a6cb4bb94cbbe55
bill <bill.mill@gmail.com>
2011-06-11 12:08:54 -0400

    explicit imports from model to save a lot of typing

--- cmua/scorereporter/models.py
+++ cmua/scorereporter/models.py
@@ -1,10 +1,10 @@
-from django.db import models
+from django.db.models import Model, CharField, ForeignKey, IntegerField, DateTimeField
 from django.contrib import admin
 
-class Team(models.Model):
-    captains = models.CharField(max_length=1024, verbose_name="Captains")
-    team_name = models.CharField(max_length=1024, verbose_name="Team Name", null=True, blank=True)
-    color = models.CharField(max_length=1024, verbose_name="Color", null=True, blank=True)
+class Team(Model):
+    captains = CharField(max_length=1024, verbose_name="Captains")
+    team_name = CharField(max_length=1024, verbose_name="Team Name", null=True, blank=True)
+    color = CharField(max_length=1024, verbose_name="Color", null=True, blank=True)
 
     def captain_or_name(self):
         return self.team_name if self.team_name else self.captains
@@ -14,19 +14,19 @@ class Team(models.Model):
 
 admin.site.register(Team)
 
-class ScoreReport(models.Model):
-    reporting_team = models.ForeignKey(Team, verbose_name="What team are you?", related_name="reporting_team")
+class ScoreReport(Model):
+    reporting_team = ForeignKey(Team, verbose_name="What team are you?", related_name="reporting_team")
 
-    opponent1 = models.ForeignKey(Team, verbose_name="Who was your opponent?", related_name="opponent1")
-    opponent2 = models.ForeignKey(Team, verbose_name="Who was your opponent?", related_name="opponent2")
+    opponent1 = ForeignKey(Team, verbose_name="Who was your opponent?", related_name="opponent1")
+    opponent2 = ForeignKey(Team, verbose_name="Who was your opponent?", related_name="opponent2")
 
-    game1_points = models.IntegerField(verbose_name="How many points did you score?")
-    game2_points = models.IntegerField(verbose_name="How many points did you score?")
+    game1_points = IntegerField(verbose_name="How many points did you score?")
+    game2_points = IntegerField(verbose_name="How many points did you score?")
 
-    opponent1_points = models.IntegerField(verbose_name="How many points did your opponent score?")
-    opponent2_points = models.IntegerField(verbose_name="How many points did your opponent score?")
+    opponent1_points = IntegerField(verbose_name="How many points did your opponent score?")
+    opponent2_points = IntegerField(verbose_name="How many points did your opponent score?")
 
-    report_date = models.DateTimeField(auto_now_add=True)
+    report_date = DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return "%s vs. %s and %s" % (self.reporting_team, self.opponent1, self.opponent2)
