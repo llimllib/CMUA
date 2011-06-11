fe31bbc85348deec8d7b1cbb89502fb18ed52c24
bill <bill.mill@gmail.com>
2011-06-11 16:38:33 -0400

    test the check_score function

--- cmua/scorereporter/tests.py
+++ cmua/scorereporter/tests.py
@@ -21,3 +21,30 @@ class GameTestCase(TestCase):
         self.assertEqual(g.team1_points, 1)
         self.assertEqual(g.team2, team2)
         self.assertEqual(g.team2_points, 2)
+
+    def test_check_score(self):
+        team1 = Team.objects.get(pk=1)
+        team2 = Team.objects.get(pk=3)
+        g = Game.objects.get(pk=2)
+
+        self.assertEqual(g.team1_points, 13)
+        self.assertEqual(g.team2_points, 1)
+
+        #first we'll check a score with a smaller diff, which should change
+        #the game's score
+        g.check_score(team1, 13, team2, 2)
+
+        #let's reload the object to make sure we didn't just change it in memory
+        g = Game.objects.get(pk=2)
+
+        self.assertEqual(g.team1_points, 13)
+        self.assertEqual(g.team2_points, 2)
+
+        #next we'll check a score with a greater diff, which should not change
+        #the game's score
+        g.check_score(team1, 13, team2, 1)
+
+        g = Game.objects.get(pk=2)
+
+        self.assertEqual(g.team1_points, 13)
+        self.assertEqual(g.team2_points, 2)
