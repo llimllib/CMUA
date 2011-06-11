a8db8429f9a791907e7c34b7767170990ce7eabc
bill <bill.mill@gmail.com>
2011-06-11 15:25:23 -0400

    moving game and team admin registrations to admin.py

--- cmua/scorereporter/admin.py
+++ cmua/scorereporter/admin.py
@@ -1,7 +1,9 @@
-from cmua.scorereporter.models import ScoreReport
+from cmua.scorereporter.models import ScoreReport, Game, Team
 from django.contrib import admin
 
 class ScoreReportAdmin(admin.ModelAdmin):
     readonly_fields = ("report_date",)
 
 admin.site.register(ScoreReport, ScoreReportAdmin)
+admin.site.register(Game)
+admin.site.register(Team)
